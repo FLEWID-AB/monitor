8e2bd280ff98b3090a17e771b6834584461ad3fe
Benjamin Betzing <benjamin.betzing@gmail.com>
2018-07-17 15:30:38 +0200

    update cfg

--- config/config.exs
+++ config/config.exs
@@ -5,33 +5,23 @@ use Mix.Config
 config :monitor,
   ets_file: "/tmp/stats_collection.tab"
 
-config :monitor, :settings,
-  environment: Atom.to_string(Mix.env),
-  hostname: "fotografiska-dev"
-
-config :monitor, Monitor.Connection,
-  host:     "192.168.178.31",
-  port_udp: 8093,
-  writer:   Instream.Writer.UDP,
-  database:  "telegraf",
-  http_opts: [ insecure: true ],
-  pool:      [ max_overflow: 0, size: 10 ],
-  port:      8093,
-  scheme:    "http"
+#config :monitor, :settings,
+#  environment: Atom.to_string(Mix.env),
+#  hostname: "fotografiska-dev"
 
 config :vmstats,
   sink: Monitor.Fluxter,
-  base_key: "vmstats_node1",
+  base_key: "vmstats_",
   key_seperator: "$", #default
   interval: 10000, # miliseconds
   sched_time: true # default
 
 
-config :fluxter,
-  host: "192.168.178.31",
-  port: 8093
+#config :fluxter,
+#  host: "192.168.178.31",
+#  port: 8093
 
-config :fluxter, MyApp.Fluxter,
-  host: "192.168.178.31",
-  port: 8093,
-  pool_size: 10
+#config :fluxter, MyApp.Fluxter,
+#  host: "192.168.178.31",
+#  port: 8093,
+#  pool_size: 10
